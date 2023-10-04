77e34b6a6f2cec891cd9bec1d3230314dccddbb5
jprodrigues644 <jprodrigues644@gmail.com>
2023-10-04 12:23:12 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ChartComponent } from './components/chart/chart.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChartComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase())
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'smartbin-c4117',
+    appId: '1:773556635919:web:88418b81481f698349967a',
+    databaseURL: 'https://smartbin-c4117-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'smartbin-c4117.appspot.com',
+    apiKey: 'AIzaSyAJD8b20HKvcmWLsghGmECRaPIzvLeYocg',
+    authDomain: 'smartbin-c4117.firebaseapp.com',
+    messagingSenderId: '773556635919',
+  },
+};
